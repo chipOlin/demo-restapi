036acd4db238fc10e346c522303c8b32e4c8ad34
Dmitry Irlitsa <chipolinn@gmail.com>
2026-03-14 00:44:56 +0300

    Finish video 002 General improvements to our first REST API

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import stores, items
 
 app = Flask(__name__)
@@ -11,6 +12,14 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message = "Bad request. Ensure 'name' is included in the JSON payload."
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message = "Store already exists.")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -21,17 +30,32 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
 
 @app.get("/item")
 def get_all_items():
     return {"items": list(items.values())}
 
 @app.post("/item")
-def create_item(name):
+def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message = "Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in item.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message = "Item already exists.")
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
@@ -42,4 +66,4 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found.")
